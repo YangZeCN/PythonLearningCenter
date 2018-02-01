a89597a63b560278f96d6e6e19dc19d308ed4ec0
YangZeCN <35066137+YangZeCN@users.noreply.github.com>
2018-02-01 22:52:17 +0800

    Rename function.py as function_global.py, add comments to code

--- function_global.py
+++ function_global.py
@@ -1,9 +1,9 @@
 '''
-Name: function.py
+Name: function_global.py
 Author: Yang Ze
 Created: 2018.01.30
 Last Modified:
-Version: V1.0
+Version: V1.1
 Description: 
 # An example to define function and compare the
 # difference between the global and local variable.
@@ -13,17 +13,15 @@ global a
 a = 10
 
 def global_variable1():
-    '''
-    Decalre a global variable, and assign a new value to a
-    '''
+    '''Decalre a global variable, and assign a new value to a'''
+    # Declare variable a is a global variable; you can declare more then one variable at one time
+    # For example: global a, b, c
     global a
     a = 2
     print(a)
 
 def global_variable2():
-    '''
-    Although the variable a was declared at begin, but in this function, it's still a local one
-    '''
+    '''Although the variable a was declared at begin, but in this function, it's still a local one'''
     a = 10
     print(a)
 
